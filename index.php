e123c4a90327d679c7e838ea6f185ca799e8d13a
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2024-06-07 20:15:43 -0400

    $_POST and $_GET

--- index.php
+++ index.php
@@ -1,40 +1,29 @@
-<?php
-
-$name = "Henry B.";
-$food = "gelato";
-$email = "email@google.com";
-$age = 21;
-$users = 2;
-$quantity = 3;
-
-$gpa = 3.0;
-$price = 4.99;
-$tax_rate = 5.1;
-
-$employed = true;
-$online = false;
-$for_sale = true;
-
-$total = null;
-
-
-echo "You have ordered $quantity x {$food}s<br>";
-$total = $quantity * $price;
-echo "Your total is \$$total.";
-?>
-
-
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>HTML 5 Boilerplate</title>
-    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
 </head>
 <body>
-<br/>
-<button>Do Something</button>
+<form action="index.php" method="post">
+    <label for="">quantity</label><br>
+    <input type="text" name="quantity">
+    <input type="submit" value="total">
+</form>
 </body>
 </html>
+
+<?php
+$item = 'pizza';
+$price = 5.99;
+$quantity = $_POST["quantity"];
+$total = null;
+
+$total = $quantity  *  $price;
+
+echo "You have ordered {$quantity} x {$item}/s<br>";
+echo "Your total is \$$total";
+
