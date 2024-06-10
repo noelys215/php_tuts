e49b6a25d6a79f1832143df4a6412bb7f523493c
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2024-06-09 21:43:37 -0400

    If Statements

--- index.php
+++ index.php
@@ -1,52 +1,34 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-<form action="index.php" method="post">
-    <label><input type="text" name="radius"></label><label>Radius: </label><br>
-
-<!--    <label><input type="text" name="y"></label><label>Y: </label><br>-->
-
-<!--    <label><input type="text" name="z"></label><label>Z: </label><br>-->
-    <input type="submit" value="calculate">
-</body>
-</html>
-
 <?php
-$radius = $_POST['radius'];
-$circumference = null;
-$area = null;
-$volume = null;
 
-$volume = 4/3 * pi() * pow($radius, 3);
-$volume = round($volume,2);
+//$age = 21;
+//$age = 15;
+//$age = -1;
+//$adult = false;
 
-$area = pi() * pow($radius, 2);
-$area = round($area, 2);
+$hours = 50;
+$rate = 15;
+$weekly_pay = null;
 
-$circumference = 2 * pi() * $radius;
-$circumference = round($circumference, 2);
+//if ($age >= 18) {
+//    echo "You may enter";
+//} elseif ($age <= 0) {
+//    echo "Invalid Age";
+//} else {
+//    echo "You may not enter";
+//}
 
-echo "Volume: $volume<br>";
-echo "Area = $area cm<br>";
-echo "Circumference: $circumference<br>";
-//$x = $_POST['x'];
-//$y = $_POST['y'];
-//$z = $_POST['z'];
-//$total = abs($x);
-//$total = round($x);
-//$total = floor($x);
-//$total = pow($x, $y);
-//$total = max($x, $y, $z);
-//$total = min($x, $y, $z);
-//$total = pi();
-//$total = rand(1, 6);
+//if ($adult){
+//    echo "You are an adult";
+//} else {
+//    echo "Your a child";
+//}
+if ($hours <= 0) {
+    $weekly_pay = 0;
+} else if ($hours <= 40) {
+    $weekly_pay = $hours * $rate;
+} else {
+    $weekly_pay = ($rate * 40) + (($hours - 40) * ($rate * 1.5));
+}
 
 
-//echo $total;
+echo "You made \${$weekly_pay} this week";
