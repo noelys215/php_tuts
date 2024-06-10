60ea02839b37a33f72a628c1219de5f6dd9be6a7
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2024-06-09 21:55:03 -0400

    More Logical Operators

--- index.php
+++ index.php
@@ -1,16 +1,15 @@
 <?php
+$age = 25;
+$citizen = true;
 
-$temp = 15;
-$cloudy = false;
+if ($age >= 18 && $citizen) echo "You can vote<br>";
+else echo "You cannot vote<br>";
 
-if ($temp <= 0 || $temp > 30) {
-    echo "The weather is bad<br>";
-} else {
-    echo "The weather is good<br>";
-}
+$child = false;
+$senior = false;
+$ticket = null;
 
-if (!$cloudy){
-    echo "It's sunny<br>";
-} else {
-    echo  "It's cloudy<br>";
-}
+if ($child || $senior) $ticket = 10;
+else $ticket = 15;
+
+echo "The ticket price is \$$ticket";
