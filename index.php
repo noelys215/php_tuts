54654dd8906f057cf34867c4b15dcefe2fba5648
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2024-06-14 20:55:59 -0400

    associative arrays

--- index.php
+++ index.php
@@ -1,23 +1,40 @@
-<?php
-$date = date("");
-//$date = "Wednesday";
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<form action="index.php" method="post">
+    <label for="">Enter a Country</label><br>
+    <input type="text" name="country"><br>
+    <input type="submit">
+</form>
+</body>
+</html>
 
-switch ($date) {
-    case "Monday":
-        echo "I hate mondays";
-        break;
-    case "Tuesday":
-        echo "Tuesdays are okay.";
-        break;
-    case "Wednesday":
-        echo "It's wednesday my dudes";
-        break;
-    case "Thursday":
-        echo "Thorsday";
-        break;
-    case "Friday":
-        echo "Friyay";
-        break;
-    default:
-        echo "Invalid Day";
-}
+<?php
+$foods = array("apples", "oranges","mango", "coconut");
+
+$capitals = array(
+    "USA" => "Washington D.C",
+    "Japan" => "Tokyo",
+    "South Korea" => "Seoul",
+    "India" => "New Delhi",
+);
+
+$capitals['USA'] = "New Jersey";
+$capitals['Puerto Rico'] = "San Juan";
+//array_shift($capitals);
+
+//foreach ($capitals as $capital => $name) echo "{$capital} | $name<br>";
+
+
+$capital = $capitals[$_POST["country"]];
+
+echo"The capital is {$capital}";
+
+?>
